5e8072b50956a8f2accc376b6a9ef6b6b562b679
Jelani Woods <jelani@firstdraft.com>
2020-07-28 16:47:50 -0500

    Add calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,14 +6,14 @@ class CalendarController < ApplicationController
   end
 
   def generate
-    @name = params.fetch("name")
-    @details = params.fetch("details")
-    @location = params.fetch("location")
+    @name = params.fetch("event_name")
+    @details = params.fetch("detail")
+    @location = params.fetch("event_location")
 
-    regular_start_date = params.fetch("start_date")
+    regular_start_date = params.fetch("start_time")
     @start_date = regular_start_date.to_s.gsub("-", "").gsub(":", "") + "00Z"
 
-    regular_end_date = params.fetch("end_date")
+    regular_end_date = params.fetch("end_time")
     @end_date = regular_end_date.to_s.gsub("-", "").gsub(":", "") + "00Z"
     
     @invite = "https://www.google.com/calendar/render?action=TEMPLATE&text=#{@name}&details=#{@details}&location=#{@location}&dates=#{@start_date}/#{@end_date}"
